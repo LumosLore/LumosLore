ff59529b2ae745081653ae6f26803a171c0b8884
AbiCosta <rhabilaash@gmail.com>
2024-03-30 14:40:31 +0530

    Questions part already committed with few changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+import './Questions.js';
 
 import Header from "./components/header";
 import Home from "./components/home";
 
 import { AuthProvider } from "./contexts/authContext";
 import { useRoutes } from "react-router-dom";
+import QuizApp from "./Questions.js";
 
 function App() {
   const routesArray = [
@@ -25,6 +27,10 @@ function App() {
       path: "/home",
       element: <Home />,
     },
+    {
+      path: "/Questions",
+      element: <QuizApp />
+    }
   ];
   let routesElement = useRoutes(routesArray);
   return (
